3e07eb197a08aa4e9f4515e06daef51bcba79977
99x0ri <168194004+99x0ri@users.noreply.github.com>
2024-10-26 21:44:12 +0300

    Create dbbackup.py

---
+++ dbbackup.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+
+"""
+Will backup all the databases listed, will put files in same DIR as script'
+To run: $ python dbbackup.py OR python3 dbbackup.py
+"""
+
+import configparser
+import os
+import time
+import getpass
+
+HOST='localhost'
+PORT='3306'
+DB_USER='tpglobalfx_u_test'
+DB_PASS='7#Dq5@^dJC8$(&A1'
+# if using one database... ('database1',)
+databases=('cloud_tpglobalfx')
+
+def get_dump(database):
+    filestamp = time.strftime('%Y-%m-%d-%I')
+    # D:/xampp/mysql/bin/mysqldump for xamp windows
+    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (HOST,PORT,DB_USER,DB_PASS,database,database+"_"+filestamp))
+    
+    print("\n|| Database dumped to "+database+"_"+filestamp+".sql || ")
+
+
+if __name__=="__main__":
+    for database in databases:
+        get_dump(database)
